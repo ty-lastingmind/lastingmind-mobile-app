40daf087152f8ab9ec3eec145c49f2fda57191d3
Zarif Abdalimov <zarif.abdalimov@gmail.com>
2025-07-19 21:47:23 +0200

    setup react-query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
 import '~/global.css'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 
+import { queryClient } from '~/libs/query-client'
 import { ColorSchemeProvider } from '~/providers/color-scheme-provider'
 import { TailwindColorsProvider, useTailwindColors } from '~/providers/tailwind-colors-provider'
 
@@ -14,7 +16,9 @@ export default function Layout() {
   return (
     <ColorSchemeProvider>
       <TailwindColorsProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </TailwindColorsProvider>
     </ColorSchemeProvider>
   )
