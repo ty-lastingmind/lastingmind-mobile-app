fb71bea0d978c78b19a33c60de66405ae023f327
Ignacio Iglesias <iiglesiascampoy@gmail.com>
2025-08-26 01:17:06 -0600

    feat: added new icons for quick action items

--- src/modules/ui/svg-icon/index.static.ts
+++ src/modules/ui/svg-icon/index.static.ts
@@ -1,5 +1,11 @@
 import { Reload } from '~/modules/ui/svg-icon/svg/reload'
+import { Interview } from '~/modules/ui/svg-icon/svg/interview'
+import { Journal } from '~/modules/ui/svg-icon/svg/journal'
+import { Chat } from '~/modules/ui/svg-icon/svg/chat'
 
 export const iconNameToSvg = {
   reload: Reload,
+  interview: Interview,
+  journal: Journal,
+  chat: Chat,
 } as const
