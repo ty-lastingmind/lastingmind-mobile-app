2d15973c9d5edbaffdbf7d48c4bf8381dbdcc8f0
Jose Francisco Alvarez Medina <alvarezmjosef.1.05@gmail.com>
2025-09-18 19:35:04 -0700

    added add audience option when audience list is empty

--- src/modules/profile/parts/user-audience/index.tsx
+++ src/modules/profile/parts/user-audience/index.tsx
@@ -1,8 +1,9 @@
-import { View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Typography } from '~/modules/ui/typography'
 import AudienceList from '../audience-list'
 import { useGetUserAudienceProfilePageGetUserAudienceGet } from '~/services/api/generated'
+import { Icon } from '~/modules/ui/icon'
 
 export default function UserAudience() {
   const { data: audienceInfo, isLoading } = useGetUserAudienceProfilePageGetUserAudienceGet()
@@ -16,7 +17,16 @@ export default function UserAudience() {
       <Typography level="h5" weight="bold">
         Audience
       </Typography>
-      {audienceInfo && <AudienceList audienceInfo={audienceInfo} />}
+      {audienceInfo ? (
+        <AudienceList audienceInfo={audienceInfo} />
+      ) : (
+        <TouchableOpacity className="p-4 items-center bg-bg-secondary rounded-xl">
+          <Icon name="add" size="3xl" />
+          <Typography level="label-1" color="secondary">
+            Add Audience
+          </Typography>
+        </TouchableOpacity>
+      )}
     </View>
   )
 }
