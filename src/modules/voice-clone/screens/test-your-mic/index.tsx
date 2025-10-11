1194484da692a91017bdd721c9531b56db3a3635
Jose Francisco Alvarez Medina <alvarezmjosef.1.05@gmail.com>
2025-10-11 14:53:20 -0700

    added basic audio visual

--- src/modules/voice-clone/screens/test-your-mic/index.tsx
+++ src/modules/voice-clone/screens/test-your-mic/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, View } from 'react-native'
+import { Alert, TouchableOpacity, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Typography } from '~/modules/ui/typography'
 import { Button } from '~/modules/ui/button'
@@ -10,10 +10,12 @@ import {
   useAudioRecorder,
   useAudioRecorderState,
 } from 'expo-audio'
+import { Icon } from '~/modules/ui/icon'
+import { RecordingWaveVisualization } from '~/modules/questions/screens/parts/recording-wave-visualization'
 
 export function VoiceCloneTestYourMic() {
   const [recordingUri, setRecordingUri] = useState<string | undefined>(undefined)
-  const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY)
+  const audioRecorder = useAudioRecorder({ ...RecordingPresets.HIGH_QUALITY, isMeteringEnabled: true })
   const recorderState = useAudioRecorderState(audioRecorder)
 
   const player = useAudioPlayer(recordingUri)
@@ -35,25 +37,22 @@ export function VoiceCloneTestYourMic() {
     setPermission()
   }, [])
 
-  const record = async () => {
+  const record = () => {
     audioRecorder.record()
   }
 
   const stopRecording = async () => {
-    console.log({ recorderState, audioRecorder })
     await audioRecorder.stop()
     setRecordingUri(audioRecorder.uri || undefined)
   }
 
   const playRecording = () => {
-    console.log({ recorderState, audioRecorder })
     player.seekTo(0)
     player.play()
   }
 
   const pauseRecording = () => {
-    console.log({ recorderState, audioRecorder })
-    audioRecorder.pause()
+    player.pause()
   }
 
   return (
@@ -64,13 +63,40 @@ export function VoiceCloneTestYourMic() {
         </Typography>
         <Typography color="secondary">Tap the Top of Your Mic and make sure the wavelength below responds</Typography>
       </View>
-      <View className="flex-1 items-center justify-center">
-        <Button onPress={recorderState.isRecording ? stopRecording : record}>
-          {recorderState.isRecording ? 'Stop Recording' : 'Start Recording'}
-        </Button>
-        <Button onPress={player.paused ? playRecording : pauseRecording}>
-          {player.paused ? 'Play Recording' : 'Pause Recording'}
-        </Button>
+      <View className="flex-1 py-12">
+        <View className="flex-1 justify-center">
+          <RecordingWaveVisualization duration={recorderState.durationMillis} metering={recorderState.metering} />
+        </View>
+        <View className="items-center justify-evenly flex-row">
+          <TouchableOpacity
+            onPress={player.paused ? playRecording : pauseRecording}
+            disabled={!recordingUri}
+            className="w-20 h-20 rounded-full items-center justify-center border-2 border-fill-accent mt-4"
+          >
+            {player.paused ? (
+              <Icon name="play" color="accent" size="lg" />
+            ) : (
+              <Icon name="pause" color="accent" size="lg" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={recorderState.isRecording ? stopRecording : record}
+            className="w-24 h-24 rounded-full items-center justify-center bg-fill-accent"
+          >
+            {recorderState.isRecording ? (
+              <Icon name="ellipse" color="white" size="xl" />
+            ) : (
+              <Icon name="square" color="white" size="xl" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={player.paused ? playRecording : pauseRecording}
+            disabled={!recordingUri}
+            className="w-20 h-20 rounded-full items-center justify-center border-2 border-fill-accent mt-4"
+          >
+            <Icon name="arrow-redo" size="lg" color="accent" />
+          </TouchableOpacity>
+        </View>
       </View>
       <Button>Continue</Button>
     </View>
