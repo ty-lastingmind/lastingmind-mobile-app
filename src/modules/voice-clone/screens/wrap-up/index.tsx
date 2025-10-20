7a29320a53a2c8c27c609cdebbbadbd2fe47a359
Jose Francisco Alvarez Medina <alvarezmjosef.1.05@gmail.com>
2025-10-20 15:49:50 -0700

    added process-voice-clone call to wrap-up

--- src/modules/voice-clone/screens/wrap-up/index.tsx
+++ src/modules/voice-clone/screens/wrap-up/index.tsx
@@ -2,10 +2,27 @@ import { View } from 'react-native'
 import React from 'react'
 import { Typography } from '~/modules/ui/typography'
 import { SvgIcon } from '~/modules/ui/svg-icon'
-import { Link } from 'expo-router'
+import { useRouter } from 'expo-router'
 import { Button } from '~/modules/ui/button'
+import { useBoolean } from 'usehooks-ts'
+import { processVoiceCloneVoiceCloneProcessVoiceCloneGet } from '~/services/api/generated'
 
 export function VoiceCloneWrapUp() {
+  const isLoading = useBoolean(false)
+  const router = useRouter()
+
+  const handleCreateVoiceClone = () => {
+    if (isLoading.value) return
+    isLoading.setTrue()
+    processVoiceCloneVoiceCloneProcessVoiceCloneGet()
+      .then(() => {
+        router.replace('/(protected)/(tabs)/home')
+      })
+      .finally(() => {
+        isLoading.setFalse()
+      })
+  }
+
   return (
     <View className="p-10 flex-1">
       <View className="gap-4">
@@ -17,9 +34,9 @@ export function VoiceCloneWrapUp() {
       <View className="items-center justify-center flex-1 mb-12">
         <SvgIcon name="mic_test" size="logo" color="accent" />
       </View>
-      <Link asChild href="/(protected)/(tabs)/home">
-        <Button>Create Voice Clone</Button>
-      </Link>
+      <Button onPress={handleCreateVoiceClone} loading={isLoading.value}>
+        Create Voice Clone
+      </Button>
     </View>
   )
 }
