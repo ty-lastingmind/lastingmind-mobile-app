eea3b235230ba1dfc697a9f56b8c4ffb335dea14
RobertoBustos <98673170+RobertoBustos@users.noreply.github.com>
2025-10-20 06:38:54 -0600

    fixed settings provider and data refreshing on account screen

--- src/modules/settings/components/common/screen-layout/index.tsx
+++ src/modules/settings/components/common/screen-layout/index.tsx
@@ -1,6 +1,5 @@
 import React, { PropsWithChildren } from 'react'
 import { SafeAreaView, View } from 'react-native'
-import { SettingsProvider } from '~/modules/settings/contexts/settings-context'
 import { SettingsHeader } from '../screen-header'
 
 export type SettingsScreenLayoutProps = PropsWithChildren<{
@@ -9,11 +8,9 @@ export type SettingsScreenLayoutProps = PropsWithChildren<{
 
 export function SettingsScreenLayout({ children, title }: SettingsScreenLayoutProps) {
   return (
-    <SettingsProvider>
-      <SafeAreaView className="flex-1bg-bg-primary">
-        <SettingsHeader title={title} />
-        <View className="p-[16px] gap-[24px]">{children}</View>
-      </SafeAreaView>
-    </SettingsProvider>
+    <SafeAreaView className="flex-1bg-bg-primary">
+      <SettingsHeader title={title} />
+      <View className="p-[16px] gap-[24px]">{children}</View>
+    </SafeAreaView>
   )
 }
--- src/modules/settings/screens/account-screen/index.tsx
+++ src/modules/settings/screens/account-screen/index.tsx
@@ -1,8 +1,20 @@
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback } from 'react'
 import { SettingsScreenLayout } from '../../components/common/screen-layout'
+import { useSettings } from '../../contexts/settings-context'
 import { AccountForm } from '../../parts/account-form'
 import { AccountFormActions } from '../../parts/account-form-actions'
 
 export function AccountScreen() {
+  const { fetchUserSettings } = useSettings()
+
+  // Refresh user settings when screen comes into focus
+  useFocusEffect(
+    useCallback(() => {
+      fetchUserSettings()
+    }, [fetchUserSettings])
+  )
+
   return (
     <SettingsScreenLayout title="Account">
       <AccountForm />
