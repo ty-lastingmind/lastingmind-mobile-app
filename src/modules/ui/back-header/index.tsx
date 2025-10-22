04ff255a9e5277ab4b7feec252202ec296ca82c2
Jose Francisco Alvarez Medina <alvarezmjosef.1.05@gmail.com>
2025-10-21 20:44:53 -0700

    added safeareaview to header

--- src/modules/ui/back-header/index.tsx
+++ src/modules/ui/back-header/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useRouter } from 'expo-router'
 import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { Logo } from '~/modules/components/logo'
 import { Icon } from '~/modules/ui/icon'
 import { Typography } from '~/modules/ui/typography'
@@ -12,18 +13,20 @@ interface BackHeaderProps {
 export default function BackHeader({ title }: BackHeaderProps) {
   const router = useRouter()
   return (
-    <View className="pb-4 px-8 flex-row items-center justify-between">
-      <TouchableOpacity className="w-[38px]" onPress={router.back}>
-        <Icon name="chevron-back" size="xl" color="accent" />
-      </TouchableOpacity>
-      <Typography brand color="accent" level="h5">
-        {title}
-      </Typography>
-      <Link asChild href="/(protected)/(tabs)/home">
-        <TouchableOpacity>
-          <Logo />
+    <SafeAreaView>
+      <View className="pb-4 px-8 flex-row items-center justify-between">
+        <TouchableOpacity className="w-[38px]" onPress={router.back}>
+          <Icon name="chevron-back" size="xl" color="accent" />
         </TouchableOpacity>
-      </Link>
-    </View>
+        <Typography brand color="accent" level="h5">
+          {title}
+        </Typography>
+        <Link asChild href="/(protected)/(tabs)/home">
+          <TouchableOpacity>
+            <Logo />
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </SafeAreaView>
   )
 }
